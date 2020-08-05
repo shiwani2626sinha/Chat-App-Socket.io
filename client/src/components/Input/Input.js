b9c4e9e57a6ee132ad1288d6cd5d74861a2463fd
shiwani2626sinha <shiwani2626@gmail.com>
2020-08-05 16:44:41 +0530

    last

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Input.css'
+import InfoBar from '../InfoBar/InfoBar';
+
+const Input = ({ setMessage, sendMessage, message }) => (
+<form className="form">
+    <input
+    className="input"
+    type="text"
+    value={message}
+    placeholder="Type a message..."
+    onChange = {(event) => setMessage(event.target.value)}
+    onKeyPress={event => event.key === 'Enter' ? sendMessage(event):null}
+    />
+    <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+</form>
+)
+
+
+export default Input
--- server/index.js
+++ server/index.js
@@ -1,25 +1,51 @@
-const express = require('express');
-const socketio = require('socket.io'); // data transfer app, real time analytics
 const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server); // rundown to  make this socket server working
+const io = socketio(server);
 
-io.on('connection', (socket) => { // io is a socket.io server instance  and socket argument is an object that represents an incoming socket connection from client
-    console.log("we have a new connection")
-
-    socket.on('disconnect', () => {
-        console.log('User left')
-    })
-});
-
+app.use(cors());
 app.use(router);
 
-server.listen(PORT, () => {
-    console.log(`server has started on port ${PORT}`)
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
+
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
